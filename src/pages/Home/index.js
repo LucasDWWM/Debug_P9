aa8b95550bef46db8076a5885e105226eb6a7e2c
Lucasi <lucaslipari06@gmail.com>
2025-07-25 09:52:16 +0200

    save

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -115,20 +115,14 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre dernière prestation</h3>
-          {last && last.cover && last.title && last.date ? (
-            <EventCard
-              imageSrc={last.cover}
-              title={last.title}
-              date={new Date(last.date)}
-              small
-              label={last.type || "Prestation"}
-            />
-          ) : (
-            <div className="no-presta">
-              <p>Aucune prestation récente</p>
-            </div>
-          )}
+        <h3>Notre derniére prestation</h3>
+        <EventCard
+          imageSrc={last?.cover}
+          title={last?.title}
+          date={new Date(last?.date)}
+          small
+          label="boom"
+        />
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
